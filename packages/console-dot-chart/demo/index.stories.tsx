44a388858f310f12d708ad61be974c9a43e2fde3
seekhow.qh <seekhow.qh@alibaba-inc.com>
2019-12-05 10:34:56 +0800

    docs: update console-dot-chart

--- packages/console-dot-chart/demo/index.stories.tsx
+++ packages/console-dot-chart/demo/index.stories.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Basic from './basic';
+import Basic from './Basic';
+import Multi from './Multi';
+import Size from './Size';
+import Disturbance from './Disturbance';
 
 storiesOf('ConsoleDotChart', module)
-  .add('基本用法', () => <Basic />);
+  .add('基本散点图', () => <Basic />)
+  .add('多组数据', () => <Multi />)
+  .add('自定义点大小', () => <Size />)
+  .add('扰动图', () => <Disturbance />);
