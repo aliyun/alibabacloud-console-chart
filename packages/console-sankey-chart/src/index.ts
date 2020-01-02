cfa814031c17296b91ec6088b612db16f39569b6
seekhow.qh <seekhow.qh@alibaba-inc.com>
2020-01-02 09:46:07 +0800

    fix: remove data-set

--- packages/console-sankey-chart/src/index.ts
+++ packages/console-sankey-chart/src/index.ts
@@ -2,15 +2,16 @@ import {
   Utils,
   getConsoleConfig,
   g2Factory,
-  // g2Tooltip,
-  // g2Legend,
-  // g2Label,
-  // g2Size,
-  // g2Style,
-  // Constants,
-  // dataAdapter,
+  g2Tooltip,
+  g2Legend,
+  g2Label,
+  g2Size,
+  g2Style,
+  Constants,
 } from '@alicloud/console-shared-utils';
-import DataSet from '@antv/data-set';
+// import DataSet from '@antv/data-set';
+
+import transform from './transform';
 
 const COMPONENT_NAME = 'ConsoleSankeyChart';
 const cfg = {
@@ -31,23 +32,16 @@ const cfg = {
   drawChart(chart, config, data) {
     const nodesData = data.find(x => x.type === 'node') || {};
     const linksData = data.find(x => x.type === 'link') || {};
-    const dataMap = {
+    const adaptData = {
       nodes: nodesData.data || [],
       edges: linksData.data || [],
     };
-    const ds = new DataSet();
-    const adaptData = ds.createView().source(dataMap, {
-      type: 'graph',
-    });
-    adaptData.transform({
-      type: 'diagram.sankey'
-    });
-    // const adaptData = dataAdapter(data, config);
-    // chart.source(adaptData);
 
-    // g2Legend(chart, config, config.legend);
+    transform(adaptData, {});
 
-    // g2Tooltip(chart, config, config.tooltip, { showTitle: false });
+    g2Legend(chart, config, config.legend);
+
+    g2Tooltip(chart, config, config.tooltip);
 
     chart.axis(false);
     chart.scale({
@@ -60,48 +54,31 @@ const cfg = {
     edgeView.edge()
       .position('x*y')
       .shape('arc')
-      .color('#bbb')
-      .opacity(0.6)
-      .tooltip('target*source*value', (target, source, value) => {
-        return {
-          name: source.name + ' to ' + target.name + '</span>',
-          value
-        };
-      });
+      .color('#ECECEC')
+      .opacity(0.6);
+
+    g2Size(edgeView, config, config.size);
+    g2Style(edgeView, config, config.style);
+    g2Label(edgeView, config, config.label);
 
     // node view
     const nodeView = chart.view();
+
+    const colors = config.colors || Utils.get(this, 'consoleTheme.colors_24') || Constants.COLORS_24;
+
     nodeView.source(adaptData.nodes);
     nodeView.polygon()
       .position('x*y') // nodes数据的x、y由layout方法计算得出
-      .color('name')
-      .label('name', {
-        textStyle: {
-          fill: '#545454',
-          textAlign: 'start'
-        },
-        offset: 0,
-        formatter: val => {
-          return '  ' + val;
-        }
-      })
-      .tooltip(false)
+      .color('name', colors)
       .style({
         stroke: '#ccc'
       });
     chart.render();
 
-    // const colors = config.colors || Utils.get(this, 'consoleTheme.colors_24') || Constants.COLORS_24;
-
-    // const geom = chart
-    //   .interval()
-    //   .position('x*y')
-    //   .color('x', colors);
-
-    // g2Size(geom, config, config.size);
-    // g2Style(geom, config, config.style);
-    // g2Label(geom, config, config.label);
-    // chart.render();
+    g2Size(nodeView, config, config.size);
+    g2Style(nodeView, config, config.style);
+    g2Label(nodeView, config, config.label);
+    chart.render();
   },
 };
 
