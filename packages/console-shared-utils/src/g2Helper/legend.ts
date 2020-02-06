62a10fb6aa2481db112367594a1a684133604c9f
seekhow.qh <seekhow.qh@alibaba-inc.com>
2020-02-06 11:10:40 +0800

    feature(legend): legend tip属性支持

--- packages/console-shared-utils/src/g2Helper/legend.ts
+++ packages/console-shared-utils/src/g2Helper/legend.ts
@@ -1,4 +1,4 @@
-import { get } from '../common/utils';
+import { get, isFunction } from '../common/utils';
 
 export default function(chart, config, legendConfig, yField?: any) {
   let finalYField = yField || true;
@@ -15,6 +15,25 @@ export default function(chart, config, legendConfig, yField?: any) {
       chart.legend(false);
     }
     return;
+  }
+  if (get(finalLegendConfig, 'tip')) {
+    const dom = get(chart, '_attrs.canvas._cfg.canvasDOM');
+    dom.title = null;
+    const onHover = event => {
+      const value: string = get(event, 'item.value');
+      let text: string = value;
+      if (get(config, 'legend.tipFormatter') && isFunction(config.legend.tipFormatter)) {
+        text = config.legend.tipFormatter(get(event, 'item.value'));
+      } else if (get(config, 'legend.itemFormatter') && isFunction(config.legend.itemFormatter)) {
+        text = config.legend.itemFormatter(get(event, 'item.value'));
+      }
+      dom.title = text;
+    };
+    if (Array.isArray(legendConfig)) {
+      chart.legend(finalYField, { onHover });
+    } else {
+      chart.legend({ onHover });
+    }
   }
 
   finalLegendConfig = Object.assign({}, { position: 'bottom-center' }, finalLegendConfig);
