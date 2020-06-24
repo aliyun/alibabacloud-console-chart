4e3054ef145e675b4a589688bd7e595d28604a16
seekhow.qh <seekhow.qh@alibaba-inc.com>
2020-06-24 10:26:48 +0800

    docs: GradualColor demo fix

--- packages/console-line-chart/demo/GradualColor.tsx
+++ packages/console-line-chart/demo/GradualColor.tsx
@@ -14,6 +14,21 @@ const config = {
   smooth: true,
 };
 
+const onTooltipChange = evt => {
+  let items = evt.items; // tooltip显示的项，不处理的话会有2项
+  // 在这里 去掉你想去掉的item，这里选择只要纯色的（线的颜色）
+  const newItems = items.filter(x => x.color === '#0072ff');
+  items.splice(0); // 将原来的Tooltip Items清空
+  items.push(newItems[0]); // 只展示你筛选后的结果
+}
+
 export default () => (
-  <ConsoleLineChart data={data} config={config} height={300} />
+  <ConsoleLineChart
+    data={data}
+    config={config}
+    height={300}
+    event={{
+      'tooltip:change': onTooltipChange,
+    }}
+  />
 );
